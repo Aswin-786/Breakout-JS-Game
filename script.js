53a5c6f2df44148c6b29d0055210138467e14a99
Aswin <aswinkrishna363@gmail.com>
2023-02-18 18:13:41 +0530

    user created and blocks added

--- script.js
+++ script.js
@@ -1,1 +1,115 @@
+const grid = document.querySelector('.game-box')
+const blockWidth = 100
+const blockHeight = 20
 
+const boardWidth = 560
+const boardHeight = 300
+
+const userStart = [230,10]
+let currentPosition = userStart
+
+const ballStart = [270,40]
+let ballCurrentPositon = ballStart
+
+// class bllock
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis,yAxis]
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    this.topLeft = [xAxis, yAxis + blockHeight]
+  }
+}
+
+const blocks = [
+  new Block(10,270),
+  new Block(120,270),
+  new Block(230,270),
+  new Block(340,270),
+  new Block(450,270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210)
+]
+
+// to draw block
+const addBlocks = () => {
+  for(let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomLeft[0] + 'px'
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+    grid.appendChild(block)
+  
+  }
+}
+
+addBlocks()
+
+
+
+// draw user 
+const drawUser = () => {
+  user.style.left = currentPosition[0] + 'px'
+  user.style.bottom = currentPosition[1] + 'px'
+}
+
+// draw ball 
+const drawBall = () => {
+  ball.style.left = ballCurrentPositon[0] + 'px'
+  ball.style.bottom = ballCurrentPositon[1] + 'px'
+
+}
+
+// add user
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+// move user
+const moveUser = (e) => {
+   switch (e.key) {
+    case 'ArrowLeft':
+      if(currentPosition[0] > 0) {
+        currentPosition[0] -= 10
+        drawUser()
+      }
+      break;
+
+      case 'ArrowRight':
+        if(currentPosition[0] < boardWidth - blockWidth ) {
+          currentPosition[0] += 10
+          drawUser()
+        }
+        break;
+   }
+}
+
+
+// add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+
+// move ball
+const moveBall = () => {
+   ballCurrentPositon[0] += 2
+   ballCurrentPositon[1] += 2
+   drawBall()
+}
+
+setInterval(moveBall,30)
+
+
+
+document.addEventListener('keydown', moveUser)
