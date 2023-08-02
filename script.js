3bf10f6817b5208f0e5dba682d8d7f74fa0987c7
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-08-02 21:18:17 +0530

    Everything related  to format

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const grid = document.querySelector('.game-box')
-const blockWidth = 100
-const blockHeight = 20
+const grid = document.querySelector('.game-box');
+const blockWidth = 100;
+const blockHeight = 20;
 
-const boardWidth = 560
-const boardHeight = 300
+const boardWidth = 560;
+const boardHeight = 300;
 
-const userStart = [230,10]
-let currentPosition = userStart
+const userStart = [230, 10];
+let currentPosition = userStart;
 
-const ballStart = [270,40]
-let ballCurrentPositon = ballStart
+const ballStart = [270, 40];
+let ballCurrentPosition = ballStart;
 
-// class bllock
+// Class Block
 class Block {
   constructor(xAxis, yAxis) {
-    this.bottomLeft = [xAxis,yAxis]
-    this.bottomRight = [xAxis + blockWidth, yAxis]
-    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
-    this.topLeft = [xAxis, yAxis + blockHeight]
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    this.topLeft = [xAxis, yAxis + blockHeight];
   }
 }
 
 const blocks = [
-  new Block(10,270),
-  new Block(120,270),
-  new Block(230,270),
-  new Block(340,270),
-  new Block(450,270),
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
   new Block(10, 240),
   new Block(120, 240),
   new Block(230, 240),
@@ -36,80 +36,71 @@ const blocks = [
   new Block(120, 210),
   new Block(230, 210),
   new Block(340, 210),
-  new Block(450, 210)
-]
+  new Block(450, 210),
+];
 
-// to draw block
+// Function to draw blocks
 const addBlocks = () => {
-  for(let i = 0; i < blocks.length; i++) {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.left = blocks[i].bottomLeft[0] + 'px'
-    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
-    grid.appendChild(block)
-  
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.style.left = blocks[i].bottomLeft[0] + 'px';
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+    grid.appendChild(block);
   }
-}
+};
 
-addBlocks()
+addBlocks();
 
-
-
-// draw user 
+// Function to draw user
 const drawUser = () => {
-  user.style.left = currentPosition[0] + 'px'
-  user.style.bottom = currentPosition[1] + 'px'
-}
+  user.style.left = currentPosition[0] + 'px';
+  user.style.bottom = currentPosition[1] + 'px';
+};
 
-// draw ball 
+// Function to draw ball
 const drawBall = () => {
-  ball.style.left = ballCurrentPositon[0] + 'px'
-  ball.style.bottom = ballCurrentPositon[1] + 'px'
+  ball.style.left = ballCurrentPosition[0] + 'px';
+  ball.style.bottom = ballCurrentPosition[1] + 'px';
+};
 
-}
+// Create user element
+const user = document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
 
-// add user
-const user = document.createElement('div')
-user.classList.add('user')
-drawUser()
-grid.appendChild(user)
-
-// move user
+// Function to move user
 const moveUser = (e) => {
-   switch (e.key) {
+  switch (e.key) {
     case 'ArrowLeft':
-      if(currentPosition[0] > 0) {
-        currentPosition[0] -= 10
-        drawUser()
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        drawUser();
       }
       break;
+    case 'ArrowRight':
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
+      break;
+  }
+};
 
-      case 'ArrowRight':
-        if(currentPosition[0] < boardWidth - blockWidth ) {
-          currentPosition[0] += 10
-          drawUser()
-        }
-        break;
-   }
-}
+// Create ball element
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall();
+grid.appendChild(ball);
 
-
-// add ball
-const ball = document.createElement('div')
-ball.classList.add('ball')
-drawBall()
-grid.appendChild(ball)
-
-
-// move ball
+// Function to move ball
 const moveBall = () => {
-   ballCurrentPositon[0] += 2
-   ballCurrentPositon[1] += 2
-   drawBall()
-}
+  ballCurrentPosition[0] += 2;
+  ballCurrentPosition[1] += 2;
+  drawBall();
+};
 
-setInterval(moveBall,30)
+setInterval(moveBall, 30);
 
-
-
-document.addEventListener('keydown', moveUser)
+document.addEventListener('keydown', moveUser);
